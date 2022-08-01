aa7018b9a118a304b71ee7b4eb8451de3ed6f228
xanderLum <xander.lumapac@gmail.com>
2022-08-01 19:41:24 +0800

    configure as shared library

--- vars/Main.groovy
+++ vars/Main.groovy
@@ -1,13 +1,64 @@
 package vars
 
-import constants.*
-import utility.*
 
-//import static utility.APIReqBuilder
+import constants.API
+import utility.APIReqBuilder
 
+/*
 def call() {
     println("I am in the test api stage")
     APIReqBuilder.callAPI this, API.TEST_API
 }
 
-return this
+return this*/
+
+def call(Map config) {
+    pipeline {
+        agent { label config.nodeName }
+        /*triggers {
+            //      cron('0 9,12,15 * * 1-5')
+            //trigger in 12nn and 6pm daily Monday to Friday
+            cron('0 12,18 * * 1-5')
+        }*/
+        /*options {
+            buildDiscarder(logRotator(numToKeepStr: config.numBuildToKeep))
+            skipDefaultCheckout()
+            disableConcurrentBuilds()
+        }*/
+        stages {
+
+            stage('Clean Workspace') {
+                steps {
+                    script {
+                        sh 'rm -rf *'
+                    }
+                }
+            }
+
+            stage('Checkout') {
+                steps {
+                    script {
+                        checkout scm
+                    }
+                }
+            }
+            stage('Invoke API Test') {
+                steps {
+                    script {
+                        println("I am in the test api stage")
+                        APIReqBuilder.callAPI this, API.TEST_API
+                    }
+                }
+            }
+
+
+        }
+    }
+    post {
+        always {
+            script {
+                CommonUtilities.postBuildStatusToJiraIssue this
+            }
+        }
+    }
+}
