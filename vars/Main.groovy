5fb72e3e59d027d42d629da5e84231ae647841d0
xanderLum <xander.lumapac@gmail.com>
2022-08-01 18:38:50 +0800

    clean

--- vars/Main.groovy
+++ vars/Main.groovy
@@ -2,24 +2,12 @@ package vars
 
 import constants.*
 import utility.*
+
 //import static utility.APIReqBuilder
 
 def call() {
-//    pipeline {
-        stage('Invoke test API') {
-//            script {
-                println("I am in the test api stage")
-                APIReqBuilder.callAPI this, API.TEST_API
-//            }
-        }
-        post {
-            always {
-                script {
-                    echo 'end'
-                }
-            }
-        }
-//    }
+    println("I am in the test api stage")
+    APIReqBuilder.callAPI this, API.TEST_API
 }
 
 return this
