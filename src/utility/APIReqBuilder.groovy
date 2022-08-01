a80ddf438bb95d86572e7a640961e083ce08e6ec
xanderLum <xander.lumapac@gmail.com>
2022-08-01 17:25:48 +0800

    update

--- src/utility/APIReqBuilder.groovy
+++ src/utility/APIReqBuilder.groovy
@@ -13,9 +13,9 @@ class APIReqBuilder {
         def urlBldr = new StringBuilder();
         urlBldr.append(HTTPS);
         urlBldr.append(API.TEST_CONTEXT_PATH);
-        echo 'API test url: '+urlBldr.toString();
+        println ("API test url: "+urlBldr.toString());
         def response = sh(script: 'curl -X POST -d @req.json -H \'Authorization:Token 0418bfa3937504586f4a0ea80c9fffb9\' https://xander.mocklab.io/__admin/mappings', returnStdout: true)
-        echo 'response : ' + response;
+        println ("response : " + response);
 //        return response;
     }
 }
--- vars/Main.groovy
+++ vars/Main.groovy
@@ -3,16 +3,14 @@ package vars
 import constants.*
 import utility.*
 
-//@Grab('com.github.groovy-wslite:groovy-wslite:1.1.3')
-
 def call() {
     pipeline {
-            stage('Invoke test API') {
-                    script {
-                        echo 'I am in the test api stage'
-                        APIReqBuilder.callAPI this, API.TEST_API
-                    }
+        stage('Invoke test API') {
+            script {
+                println("I am in the test api stage")
+                APIReqBuilder.callAPI this, API.TEST_API
             }
+        }
         post {
             always {
                 script {
