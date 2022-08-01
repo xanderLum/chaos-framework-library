25b6a9d006aa74f85395c43f9a4f7986e3104190
xanderLum <xander.lumapac@gmail.com>
2022-08-01 16:58:54 +0800

    retry

--- vars/Main.groovy
+++ vars/Main.groovy
@@ -1,44 +1,29 @@
 package vars
 
-import constants.*
-import utility.*
+
+import constants.API
+import utility.APIReqBuilder
 
 //@Grab('com.github.groovy-wslite:groovy-wslite:1.1.3')
-
 def call() {
     pipeline {
-        /*stages {
-            /*stage('Clean Workspace') {
+        stages {
+            stage('Invoke test API') {
                 steps {
                     script {
-                        sh 'rm -rf *'
+                        echo 'I am in the test api stage'
+                        APIReqBuilder.callAPI this, API.TEST_API
                     }
-                }
-            }
-
-            stage('Checkout') {
-                steps {
-                    script {
-                        checkout scm
-                    }
-                }
-            }*/
-        stage('Invoke test API') {
-            steps {
-                script {
-                    echo 'I am in the test api stage'
-                    APIReqBuilder.callAPI this, API.TEST_API
                 }
             }
         }
-    }
-    post {
-        always {
-            script {
-                echo 'end'
+        post {
+            always {
+                script {
+                    echo 'end'
+                }
             }
         }
     }
 }
-
 return this
