15e770406ee7ee8430986367f78a0fa2cf43a163
xanderLum <xander.lumapac@gmail.com>
2022-08-01 16:53:59 +0800

    cleanup

--- vars/Main.groovy
+++ vars/Main.groovy
@@ -5,21 +5,21 @@ import utility.*
 
 //@Grab('com.github.groovy-wslite:groovy-wslite:1.1.3')
 
-def call(Map config) {
+def call() {
     pipeline {
-        agent { label config.nodeName }
+//        agent { label config.nodeName }
         /*triggers {
   //      cron('0 9,12,15 * * 1-5')
             //trigger in 12nn and 6pm daily Monday to Friday
             cron('0 12,18 * * 1-5')
         }*/
         options {
-            buildDiscarder(logRotator(numToKeepStr: config.numBuildToKeep))
+//            buildDiscarder(logRotator(numToKeepStr: config.numBuildToKeep))
             skipDefaultCheckout()
             disableConcurrentBuilds()
         }
         stages {
-            stage('Clean Workspace') {
+            /*stage('Clean Workspace') {
                 steps {
                     script {
                         sh 'rm -rf *'
@@ -33,7 +33,7 @@ def call(Map config) {
                         checkout scm
                     }
                 }
-            }
+            }*/
             stage('Invoke test API') {
                 steps {
                     script {
