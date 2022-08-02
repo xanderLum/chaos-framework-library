f286a7ca7c09c4f330dcd9dd7fbd8759468aacf8
xanderLum <xander.lumapac@gmail.com>
2022-08-02 11:14:11 +0800

    simplify

--- vars/Orchestrator.groovy
+++ vars/Orchestrator.groovy
@@ -14,44 +14,19 @@ return this*/
 
 def call(Map config) {
     pipeline {
-//        agent { label config.nodeName }
-        /*triggers {
-            //      cron('0 9,12,15 * * 1-5')
-            //trigger in 12nn and 6pm daily Monday to Friday
-            cron('0 12,18 * * 1-5')
-        }*/
-        /*options {
-            buildDiscarder(logRotator(numToKeepStr: config.numBuildToKeep))
-            skipDefaultCheckout()
-            disableConcurrentBuilds()
-        }*/
-//        stages {
         stage('Clean Workspace') {
-            step {
-                script {
-                    sh 'rm -rf *'
-                }
-            }
+            sh 'rm -rf *'
         }
 
         stage('Checkout') {
-            step {
-                script {
-                    checkout scm
-                }
-            }
+
+            checkout scm
         }
+
         stage('Invoke API Test') {
-            step {
-                script {
-                    println("I am in the test api stage")
-                    APIReqBuilder.callAPI this, APIGroovy.TEST_API
-                }
-            }
+            println("I am in the test api stage")
+            APIReqBuilder.callAPI this, APIGroovy.TEST_API
         }
-
-
-//        }
     }
     post {
         always {
