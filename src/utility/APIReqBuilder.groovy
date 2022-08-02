d5d5d1268b55ef92d8bd17a3b8abeae8d34eef63
xanderLum <xander.lumapac@gmail.com>
2022-08-02 12:18:12 +0800

    fix path

--- src/utility/APIReqBuilder.groovy
+++ src/utility/APIReqBuilder.groovy
@@ -8,15 +8,16 @@ class APIReqBuilder {
     static String IP = "10.11.57.125"; //change to variable in pipeline
 
     static def callAPI(script, apiId) {
-        def workspace = script.WORKSPACE
+        String workspace = script.WORKSPACE
         script.sh "echo 'workspace...'"
         script.sh "echo ${workspace}"
-        
+
         script.sh "echo 'Calling api '${APIGroovy.getApiEnum(apiId)}"
 //        script.sh "curl -X POST -d '{ \"request\": { \"url\": \"/hello-world\" }, \"response\": { \"body\": \"Hi!\" }}' -H 'Authorization:Token 0418bfa3937504586f4a0ea80c9fffb9' https://xander.mocklab.io/__admin/mappings"
         def urlBldr = new StringBuilder()
         urlBldr.append(HTTPS)
         urlBldr.append(APIGroovy.TEST_CONTEXT_PATH.apiURL)
+        urlBldr.append(APIGroovy.TEST_API.apiURL)
         script.sh "echo 'API test url: '${urlBldr.toString()}"
         /*def response = sh(script: 'curl -X POST -d @req.json -H \'Authorization:Token 0418bfa3937504586f4a0ea80c9fffb9\' https://xander.mocklab.io/__admin/mappings', returnStdout: true)
         def postmanPost = new URL('https://xander.mocklab.io/__admin/mappings')
@@ -28,7 +29,7 @@ class APIReqBuilder {
         def postmanPost = new URL(urlBldr.toString());
         def postConnection = postmanPost.openConnection()
 //        def form = "param1=This is request parameter."
-        def form = [] new File('src/api/input/req.json').eachLine { line -> form.add(line) }
+        def form = [] new File(new StringBuilder(workspace).append('/src/api/input/req.json')).eachLine { line -> form.add(line) }
         postConnection.doOutput = true
         def text
         postConnection.with {
