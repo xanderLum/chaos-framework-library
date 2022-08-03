790c4ecd82be754258c620aa653710e8130de95f
xanderLum <xander.lumapac@gmail.com>
2022-08-03 11:18:35 +0800

    read content

--- src/utility/FileUtils.groovy
+++ src/utility/FileUtils.groovy
@@ -6,19 +6,23 @@ class FileUtils {
     static String FOLDER = "request/"
 
     static def processFileParam(script) {
-        script.sh "echo whereami pwd"
+        script.sh "echo pwd"
 
         if (script.fileExists(file: "${FILEPARAM}")) {
             script.sh "echo unzipping... ${FILEPARAM}"
             script.sh "unzip ${FILEPARAM}"
             script.sh 'ls'
-            script.sh "ls ${determineFaults(script, FOLDER)}"
+
+            determineFaults(script, FOLDER).each {
+                script.sh "echo content: ${FOLDER}/${it} "
+                script.sh "more ${FOLDER}/${it} "
+            }
         } else {
             script.sh "echo Fileparam doesn't exist!"
         }
     }
 
-    static def determineFaults(script, folder){
+    static def determineFaults(script, folder) {
         def foundFiles = script.sh(script: "ls -1 ${folder}", returnStdout: true).split()
         return foundFiles
     }
