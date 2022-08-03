41a3d9ed5fd2ecbb6c8cc39ae970b6119463097b
xanderLum <xander.lumapac@gmail.com>
2022-08-03 14:32:50 +0800

    Fix errors

--- vars/Orchestrator.groovy
+++ vars/Orchestrator.groovy
@@ -20,7 +20,7 @@ def call(Map config) {
         stage("Upload Request.zip file (*-req.json)") {
             def inputFile = input message: 'Please provide a file', parameters: [base64File('file')]
             withEnv(["fileBase64=$inputFile"]) {
-                if ($fileBase64 != '') {
+                if (inputFile != null) {
                     sh 'echo $fileBase64 | base64 -d > request.zip'
                 } else {
                     sh 'echo required file not provided.  throw error'
