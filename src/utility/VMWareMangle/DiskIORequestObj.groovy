1d7fa284c09efce60ab266fa4d0eff90d1b0a562
xanderLum <xander.lumapac@gmail.com>
2022-08-11 15:29:23 +0800

    fix diskio

--- src/utility/VMWareMangle/DiskIORequestObj.groovy
+++ src/utility/VMWareMangle/DiskIORequestObj.groovy
@@ -5,11 +5,13 @@ import org.apache.groovy.parser.antlr4.util.StringUtils
 
 class DiskIORequestObj extends BaseRequestObj {
     int ioSize
+    String targetDir
 
     DiskIORequestObj(String endpointName, int timeoutInMilliseconds, String id, String injectionHomeDir, String taskName, int ioSize) {
         super(endpointName, timeoutInMilliseconds, id, injectionHomeDir, taskName)
         //if ioSize is empty, default IO size is 8GB
-        this.ioSize = StringUtils.isEmpty(String.valueOf(ioSize)) ? 8e+9 : ioSize
+        this.ioSize = StringUtils.isEmpty(String.valueOf(ioSize)) ? 8e+6 : ioSize
+        this.targetDir = StringUtils.isEmpty(String.valueOf(targetDir)) ? "/home/" : targetDir
     }
 
 
@@ -17,6 +19,7 @@ class DiskIORequestObj extends BaseRequestObj {
     public String toString() {
         return "DiskIORequestObj{" + super.toString() +
                 "ioSize=" + ioSize +
+                "targetDir=" + targetDir +
                 '}';
     }
 }
